2e370233f6de9def406b308981e963419b8e5076
mamireimu <adgjkazup@yahoo.co.jp>
2017-10-10 13:13:34 +0900

    firsttest
    test

---
+++ sys/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8"/>
+</head>
+
+<body>
+
+<form method="login" action="example.cgi">
+<p>ID：<input type="text" name="name"></p>
+<p>パスワード：<input type="password" name="name"></p>
+<p><input type="submit" value="ログイン"></p>
+
+</form>
+</body>
+</html>
