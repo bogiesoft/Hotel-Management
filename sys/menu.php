057101a79898e9e13cba8381166d41d303a13b1f
mamireimu <adgjkazup@yahoo.co.jp>
2017-10-10 13:46:02 +0900

    secondtest

---
+++ sys/menu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8"/>
+</head>
+
+<body>
+<h2>メニュー</h2>
+<p>
+    <input type="button" value="予約" onclick="/Reservation/">
+</p>
+<p>
+    <input type="button" value="顧客" onclick="/client/">
+</p>
+<p>
+    <input type="button" value="前のページへ戻る" onclick="history.back()">
+</p>
+<p>
+    <input type="button" value="前のページへ戻る" onclick="history.back()">
+</p>
+
+</body>
+</html>
