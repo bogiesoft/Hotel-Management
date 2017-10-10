56d5b69040edcf5613d736d1b190557d082debba
mamireimu <adgjkazup@yahoo.co.jp>
2017-10-10 14:41:02 +0900

    5thtest

--- sys/menu.php
+++ sys/menu.php
@@ -7,19 +7,23 @@
 <body>
 <h2>全体メニュー</h2>
 <p>
-    <input type="button" value="予約" onclick="location.href='/Reservation/'">
+    <input type="button" value="予約" onclick="location.href='./Reservation/'">
 </p>
 <p>
-    <input type="button" value="顧客" onclick="location.href='/Client/'">
+    <input type="button" value="顧客" onclick="location.href='./Client/'">
 </p>
 <p>
-    <input type="button" value="従業員" onclick="location.href='/Staff/'">
+    <input type="button" value="従業員" onclick="location.href='./Staff/'">
 </p>
 <p>
-    <input type="button" value="部屋状態" onclick="location.href='/Room_status/'">
+    <input type="button" value="部屋状態" onclick="location.href='./Room_status/'">
 </p>
 <p>
-    <input type="button" value="会計" onclick="location.href='/Accounting/'">
+    <input type="button" value="会計" onclick="location.href='./Accounting/'">
+</p>
+
+<p>
+    <input type="button" value="売上情報" onclick="location.href='./Accounting/'">
 </p>
 
 </body>
