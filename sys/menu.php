1e08d9c50e2f9a548426ddb765f64b685951666f
mamireimu <adgjkazup@yahoo.co.jp>
2017-10-10 13:52:18 +0900

    thaadtest

--- sys/menu.php
+++ sys/menu.php
@@ -7,16 +7,19 @@
 <body>
 <h2>メニュー</h2>
 <p>
-    <input type="button" value="予約" onclick="/Reservation/">
+    <input type="button" value="予約" onclick="location.href='/Reservation/'">
 </p>
 <p>
-    <input type="button" value="顧客" onclick="/client/">
+    <input type="button" value="顧客" onclick="location.href='/Client/'">
 </p>
 <p>
-    <input type="button" value="前のページへ戻る" onclick="history.back()">
+    <input type="button" value="従業員" onclick="location.href='/Staff/'">
 </p>
 <p>
-    <input type="button" value="前のページへ戻る" onclick="history.back()">
+    <input type="button" value="部屋状態" onclick="location.href='/Room_status/'">
+</p>
+<p>
+    <input type="button" value="会計" onclick="location.href='/Accounting/'">
 </p>
 
 </body>
